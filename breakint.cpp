210fbc228057008e08983d72c2917ccbadef3afe
Kia Farhang <kfarhang0@gmail.com>
2016-06-23 16:47:40 -0700

    Added Chapter 1 and some Chapter 2 problems.

---
+++ breakint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Integer, please.\n";
+	string number;
+	cin >> number;
+
+	int n = number.length();
+
+	return 0;
+}
