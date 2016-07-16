09a3637fd433e011922ecd2bf8b98faaf79ea943
Kia Farhang <kfarhang0@gmail.com>
2016-07-15 20:48:27 -0700

    Solved Chapter 3 Problem 21

--- chapter_3/ex21.cpp
+++ chapter_3/ex21.cpp
@@ -10,13 +10,37 @@ int main()
 	int number;
 	cin >> number;
 
+	int counter;
+
 	cout << "Here are all the prime numbers up to your integer: \n";
 
-	/* This for statement starts at 1 and ends after reaching the given
-	integer. For each number, it checks whether the number is prime
-	and prints it if so.
+	/*
+	This problem requires a double loop. The first loop starts at the
+	given number and works down. For each iteration, it runs its own
+	loop. That secondary loop starts at 1 and checks every number
+	betwen 1 and i (the first loop counter) for its divisibility by i.
+	If j (the second loop) is only divisible by 1 and i, it's a prime
+	number and we print it. This cycle continues until i reaches 0
+	and we've checked every number below the one the user gave us.
 	*/
 
-	for (i = 1; i <= number; i++)
+	for (int i = number; i > 0; i--)
 	{
-		int check = i %
+		for (int j = 1; j <= i; j++)
+		{
+			if ((i % j) == 0)
+			{
+				counter++;
+			}
+		}
+
+		if (counter == 2)
+		{
+			cout << i << endl;
+		}
+
+		counter = 0;
+	}
+
+	return 0;
+}
