5329d6674469f7468536faa1e1649be2218a63d8
Kia Farhang <kfarhang0@gmail.com>
2016-07-16 21:36:05 -0700

    Started Chapter 3 Problem 19

---
+++ chapter_3/ex19.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string file;
+	getline(cin, file);
+
+	double words;
+	double syllables;
+	double sentences;
+
+
+	return 0;
+}
+
